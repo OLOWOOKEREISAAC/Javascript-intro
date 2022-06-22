e82d0075fb965a14aebeaf77712dd1e12c14b8f7
Olowookere I. O <100536651+OLOWOOKEREISAAC@users.noreply.github.com>
2022-06-22 11:39:33 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const myName = 'Olowookere Isaac .O.'
+let height = 6.1
+let country = ' Nigeria'
+
+intro = 'NAME : ' + myName + '   \n  HEIGHT : ' + height + ' \n  COUNTRY : ' + country
+
+document.write( intro );
+console.log(intro)
